2b3ec9e0c5aec44f65ad346b9ba758173c82a727
Abdulla Ks <abdullaks1999@gmail.com>
2022-12-10 17:56:53 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,67 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'LoginPage';
+  submitted = false;
+  submittedSignup = false;
+  hide = true;
+  hideSignup = true;
+  role = [
+    { id: 1, name: 'manager' },
+    { id: 2, name: 'hr' },
+    { id: 3, name: 'ceo' },
+    { id: 4, name: 'cto' },
+    { id: 5, name: 'dev' },
+    { id: 6, name: 'seniordev' },
+    { id: 7, name: 'tester' }
+  ];
+  constructor(private fb: FormBuilder) {}
+
+  loginForm = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  get AllControls() {
+    return this.loginForm.controls;
+  }
+
+  onSubmit(values: any) {
+    this.submitted = true;
+    console.log(values);
+  }
+  signupForm = this.fb.group(
+    {
+      name: ['', Validators.required],
+      age: [null, Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      gender: ['', Validators.required],
+      date: ['', Validators.required],
+      role: [null, Validators.required],
+      password: ['', Validators.required],
+      confirmpassword: ['', Validators.required]
+    },
+    {
+      validator: () => {
+        if (
+          this.signupForm?.controls['password'].value !=
+          this.signupForm?.controls['confirmpassword'].value
+        ) {
+          console.log("fffffffffffffffff");
+          
+          this.signupForm?.controls['confirmpassword'].setErrors({
+            paswordMismatch: true
+          });
+        }
+      }
+    }
+  );
+
+  get AllControlsSignup() {
+    return this.signupForm.controls;
+  }
+
+  onSubmitSignup(values: any) {
+    this.submittedSignup = true;
+    console.log(values);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {ReactiveFormsModule  } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MatSelectModule} from '@angular/material/select';
+ 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
